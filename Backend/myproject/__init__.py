e30684748535bdfd27a34c5bbb0c8d927cc49b7b
Brahmaji Rao <brahmaji.dnr@gmail.com>
2018-11-24 22:29:41 +0530

    get Memory service

--- Backend/myproject/__init__.py
+++ Backend/myproject/__init__.py
@@ -3,8 +3,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
 from flask_cors import CORS
+from flask import send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = 'mysecretkey'
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/thisisnenu'
@@ -16,5 +17,16 @@ api = Api(app)
 CORS(app)
 
 from myproject.services.memories import Memories
+from myproject.services.memories import Memory
 api.add_resource(Memories, '/memories')
+api.add_resource(Memory, '/memory/<string:id>')
 
+
+
+@app.route('/images/<string:item>/<string:imageName>')
+def get_image(item, imageName):
+    directory = ""
+    if item == "memories":
+        directory = "user_data/memories"
+    return send_from_directory(directory, imageName, as_attachment=False)
+    
--- Backend/myproject/services/memories.py
+++ Backend/myproject/services/memories.py
@@ -1,19 +1,46 @@
 from flask_restful import Resource
-from myproject.models.memories import Memory
+from myproject.models.memories import Memory as MemoryDb
 class Memories(Resource):
     def get(self):
-        memories = Memory.query.all()
+        memories = MemoryDb.query.all()
         output = []
         for memory in memories:
             createdDate = memory.created_date
             updatedDate = memory.updated_date
+            min_description = (memory.description[:75] + '..') if len(memory.description) > 75 else memory.description
+            thumb = 'http://127.0.0.1:5000/static/images/default_thumb.png'
+            if len(memory.cover_image)>0:
+                thumb = 'http://127.0.0.1:5000/images/memories/' + memory.cover_image
+
             outputObj = {
                 'id': memory.memory_id,
                 'title': memory.title,
-                'description': memory.description,
-                'coverImage': memory.cover_image,
+                'description': min_description,
+                'coverImage': thumb,
                 'createdDate': createdDate.strftime('%Y-%m-%d %H:%M:%S'),
                 'updatedDate': updatedDate.strftime('%Y-%m-%d %H:%M:%S')
             }
             output.append(outputObj)
+        return output
+
+class Memory(Resource):
+    def get(self, id):
+        memoryDetails = MemoryDb.query.get(id)
+        output = None
+        if memoryDetails is not None:
+            createdDate = memoryDetails.created_date
+            updatedDate = memoryDetails.updated_date
+            thumb = 'http://127.0.0.1:5000/static/images/default_thumb.png'
+            if len(memoryDetails.cover_image) > 0:
+                thumb = 'http://127.0.0.1:5000/images/memories/' + memoryDetails.cover_image
+
+            output = {
+                'id': memoryDetails.memory_id,
+                'title': memoryDetails.title,
+                'description': memoryDetails.description,
+                'coverImage': thumb,
+                'createdDate': createdDate.strftime('%Y-%m-%d %H:%M:%S'),
+                'updatedDate': updatedDate.strftime('%Y-%m-%d %H:%M:%S')
+            }
+
         return output
